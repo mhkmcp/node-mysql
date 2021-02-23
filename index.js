0618e153af924e0aebd3b6d29c8e75c596028aab
mhkmcp <mhkmcp@yahoo.com>
2021-02-24 00:53:17 +0600

    02: Insert Data

--- index.js
+++ index.js
@@ -8,5 +8,18 @@ con.connect(function (error) {
         console.log("Connection Failed!");
     } else {
         console.log("Connection Successful!");
+        InsertData(con);
     }
-})
+});
+
+function InsertData(con) {
+    let query = "INSERT INTO `student`(`name`, `roll`, `class`, `phone`, `city`, `gurdian`) VALUES ('Humayun Kabir', '140108','Bachelor','242482','Sylhet','Prince')";
+    con.query(query, function (error) {
+        if (error) {
+            console.log('Insert Failed!');
+        } else {
+            console.log("Insert Success");
+        }
+    });
+
+}
